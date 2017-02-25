735417b6b71b7353c2a1da636b8d7ea9b01978c3
Topmost <topmost.com.au@gmail.com>
2017-02-25 19:14:03 +1100

    minor updates

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 docker rm -f jekyllserver
 docker build -t jekylltopmost .
-docker run -d -p 4500:4000 -v $(pwd):/topmost --name jekyllserver jekylltopmost:latest
+docker run -d -p 4000:4000 -v $(pwd):/topmost --name jekyllserver jekylltopmost:latest
